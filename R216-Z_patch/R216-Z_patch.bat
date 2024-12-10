1569a49762f858ce5a6c15c48415a4e3c1be6542
cosote <cosote@gmail.com>
2024-12-10 09:52:04 +0100

    Add reboot on internet loss

--- R216-Z_patch/R216-Z_patch.bat
+++ R216-Z_patch/R216-Z_patch.bat
@@ -4,37 +4,59 @@ set curl=curl\curl.exe
 
 echo Verify login with password admin on R216-Z...
 %curl% -e "http://192.168.0.1/home.htm" -d "goformId=LOGIN" -d "password=YWRtaW4=" "http://192.168.0.1/goform/goform_set_cmd_process"
-echo.
 if errorlevel 1 goto :cannot_connect
+
 echo.
 echo Login with password admin on R216-Z...
 %curl% -c cookies.txt -e "http://192.168.0.1/home.htm" -d "goformId=LOGIN_EXCLUSIVE" -d "password=YWRtaW4=" "http://192.168.0.1/goform/goform_set_cmd_process"
-echo.
 if errorlevel 1 goto :cannot_connect
+
 echo.
 echo Enable ADB connection on R216-Z...
 %curl% -b cookies.txt -e "http://192.168.0.1/home.htm" -d "goformId=USB_MODE_SWITCH" -d "usb_mode=6" "http://192.168.0.1/goform/goform_set_cmd_process"
-echo.
 if errorlevel 1 goto :cannot_connect
+
 echo.
 echo Wait for R216-Z...
 %adb% wait-for-device
+echo Logoff admin on R216-Z...
+%curl% -e "http://192.168.0.1/home.htm" -d "goformId=LOGOUT" "http://192.168.0.1/goform/goform_set_cmd_process"
 if "%1" == "remove" goto remove
+
+echo.
+set reboot=patch_end
+choice /m "Do you want to deploy the re-connect script"
+if errorlevel 2 goto :auto_reconnect_patch
+goto :patch_start
+
+:patch_start
+set end=patch_end
 echo.
 echo Push patch to R216-Z...
 for /f "tokens=*" %%a in ('%adb% shell mkdir /usr/ui/^|find /i "File exists"') do (
-	if not errorlevel 1 goto :cannot_patch
+	if not errorlevel 1 goto :already_patched
 )
 :continue_patch
+echo.
 %adb% push startdui.sh /usr/ui/
 %adb% push ppp_reconnect.sh /etc/
+set reboot=reboot
+goto :auto_reconnect_patch
+
+:auto_reconnect_patch
 echo.
-echo Done. Please reboot R216-Z.
+echo Additional mobile networks, like Telekon, Telefonica, o2 and 1^&1
+echo can be added to enable auto-reconnect.
+choice /m "Do you want to update the apn_list database"
+if errorlevel 2 goto :%reboot%
+rem Create backup of database
+%adb% shell if ! [ -f "/usr/zte_web/web/auto_apn/auto_apn_backup.db" ]; then cp /usr/zte_web/web/auto_apn/auto_apn.db /usr/zte_web/web/auto_apn/auto_apn_backup.db; fi
 echo.
-choice /m "Do you want to reboot the R216-Z"
-if errorlevel 2 goto :patch_end
-echo Rebooting R216-Z...
-%adb% shell reboot
+%adb% push apn_list_update.sh /etc/
+%adb% shell sh /etc/apn_list_update.sh add
+%adb% shell sh /etc/apn_list_update.sh list
+goto :reboot
+
 :patch_end
 echo.
 echo Info:
@@ -42,27 +64,38 @@ echo Run command: %0 remove
 echo to remove patch from R216-Z again.
 goto :END
 
-:cannot_patch
-echo Error: Cannot create directory!
+:already_patched
+echo.
 echo Patch is already installed or system files exist and might be overwritten.
 choice /m "Do you want to continue"
 if errorlevel 2 goto :END
 goto :continue_patch
 
 :cannot_connect
-echo Error: Cannot connect to R216-Z!
+echo.
+echo Error: Cannot connect to R216-Z! Please ensure you're connected to its WLAN!
 goto :END
 
 :remove
 echo.
 echo Remove patch from R216-Z...
-%adb% shell rm /usr/ui/startdui.sh 
-%adb% shell rm /etc/ppp_reconnect.sh 
-%adb% shell rmdir /usr/ui/
 %adb% shell pkill -f "sh /etc/ppp_reconnect.sh"
+%adb% shell if [ -f "/usr/ui/startdui.sh" ]; then rm /usr/ui/startdui.sh; fi
+%adb% shell if [ -f "/etc/ppp_reconnect.sh" ]; then rm /etc/ppp_reconnect.sh; fi
+%adb% shell if [ -d "/usr/ui/" ]; then rmdir /usr/ui/; fi
+%adb% shell if [ -f "/etc/apn_list_update.sh" ]; then rm /etc/apn_list_update.sh; fi
+%adb% shell if [ -f "/usr/zte_web/web/auto_apn/auto_apn_backup.db" ]; then cp /usr/zte_web/web/auto_apn/auto_apn_backup.db /usr/zte_web/web/auto_apn/auto_apn.db;rm /usr/zte_web/web/auto_apn/auto_apn_backup.db; fi
+set end=END
+goto :reboot
+
+:reboot
 echo.
-echo Done.
-goto :END
+echo Done. Please reboot R216-Z.
+echo.
+choice /m "Do you want to reboot the R216-Z"
+if errorlevel 2 goto :%end%
+echo Rebooting R216-Z...
+%adb% shell reboot
 
 :END
 echo.
--- R216-Z_patch/ppp_reconnect.sh
+++ R216-Z_patch/ppp_reconnect.sh
@@ -6,30 +6,107 @@
 # Default-Start:     2 3 4 5
 # Default-Stop:
 # Short-Description: Connects network at boot for providers other than Vodafone
-# Description:       Checks every 3 seconds on disconnected status and issues connect. Uses busybox nc.
+# Description:       Check connection and connects or reboots device. Uses busybox nc.
 ### END INIT INFO
 
 # Author: 2020-05-02 cosote@gmail.com
+# Update: 2024-12-06 Added smart reboot on internet loss
 
-EPOCH=0
+# Primary check, if internet is accessible
+MYIP_SITE=http://icanhazip.com
+# Alternative check, if internet is accessible
+ALT_SITE=http://www.google.com
+# Sleep inside loop
+LOOP_SLEEP_SEC=5
+# Additional check, that mobile connection is established
+CONNECT_CHECK_SEC=120 # 2 Minutes
+# Additional check, that internet is accessible
+INTERNET_CHECK_SEC=300 # 5 Minutes
+INTERNET_LONG_CHECK_SEC=1200 # 20 Minutes
+INTERNET_ERROR_REBOOT_AT=3 # Reboot at 3 internet errors
+# When device rebooted and cannot connect to internet, it will automatically reboot at
+# $INTERNET_CHECK_SEC + $INTERNET_LONG_CHECK_SEC * ($INTERNET_ERROR_REBOOT_AT - 1), so approx. 45 Minutes
+# When a internet connection had been established and stopped, it will reboot within 15 Minutes
+HAD_ONCE_INTERNET=-1 # -1 for unknown, 0 for never had internet, 1 for internet access existed
+
+NOW=$(date +%s)
+echo $NOW: Running $0
+
+LAST_CYCLE=0
+LAST_CONNECT_CHECK=0
+LAST_INTERNET_CHECK=$(date +%s)
+INTERNET_ERROR=0
+REBOOT_CHECK_SEC=$(($LOOP_SLEEP_SEC + 1))
+
+HOST="127.0.0.1"
+H_HOST="Host: $HOST"
+H_REFERER="Referer: http://$HOST/home.htm"
+H_CONTENTTYPE="Content-Type: application/x-www-form-urlencoded"
+
+check_network_status() {
+  RESULT=$( echo -ne "GET /goform/goform_get_cmd_process?cmd=ppp_status HTTP/1.0\n$H_HOST\n$H_REFERER\n$H_CONTENTTYPE\n\n" | nc $HOST 80 | grep -o "{.*}" )
+  echo $NOW: $RESULT
+  if [[ "$RESULT" = '{"ppp_status":"ppp_disconnected"}' ]]; then
+    return 1
+  else
+    return 0
+  fi
+}
 
 while [ 1 ]
 do
-   sleep 3
-   
-   # Check if system was rebooted or resumed from suspension
-   if [[ $(($(date +%s) - $EPOCH)) -gt 5 ]]; then
-   
-      # Check network
-      RESULT=$( echo -ne "GET /goform/goform_get_cmd_process?cmd=ppp_status HTTP/1.0\r\nHost: 127.0.0.1\r\nReferer: http://127.0.0.1/home.htm\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n" | nc 127.0.0.1 80 | grep "ppp_disconnected" )
-      
-      if [[ "$RESULT" = '{"ppp_status":"ppp_disconnected"}' ]]; then
-        # Connect network
-        BODY="goformId=CONNECT_NETWORK"
-        RESULT=$( echo -ne "POST /goform/goform_set_cmd_process HTTP/1.0\r\nHost: 127.0.0.1\r\nReferer: http://127.0.0.1/home.htm\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: $( echo -n ${BODY} | wc -c )\r\n\r\n${BODY}" | nc 127.0.0.1 80 )
+  sleep $LOOP_SLEEP_SEC
+
+  # Check if system was rebooted or resumed from suspension or every 2 Minutes, that connection is established
+  NOW=$(date +%s)
+  if [[ $(($NOW - $LAST_CYCLE)) -gt $REBOOT_CHECK_SEC ]] || [[ $(($NOW - $LAST_CONNECT_CHECK)) -gt $CONNECT_CHECK_SEC ]]; then
+    # Check network
+    NOT_CONNECTED_FIX=0
+    if ! check_network_status; then
+      # Connect network
+      # NOTE: Occasionally it may return "{"result":"success"}" but is not true!
+      BODY="goformId=CONNECT_NETWORK"
+      RESULT=$( echo -ne "POST /goform/goform_set_cmd_process HTTP/1.0\n$H_HOST\n$H_REFERER\n$H_CONTENTTYPE\nContent-Length: ${#BODY}\n\n${BODY}" | nc $HOST 80 | grep -o "{.*}" )
+      echo $NOW: Connect command result: $RESULT
+      # So, Re-check network status
+      if ! check_network_status; then
+        # check again on next cycle
+        NOT_CONNECTED_FIX=$(($CONNECT_CHECK_SEC - 1))
       fi
-   fi
-   
-   EPOCH=$(date +%s)
-   
+    fi
+    LAST_CONNECT_CHECK=$(($NOW - $NOT_CONNECTED_FIX))
+  fi
+
+  # check internet and reboot if failing multiple times
+  if ([[ $(($NOW - $LAST_INTERNET_CHECK)) -gt $INTERNET_CHECK_SEC ]] && [[ $HAD_ONCE_INTERNET -ne 0 ]]) || ([[ $(($NOW - $LAST_INTERNET_CHECK)) -gt $INTERNET_LONG_CHECK_SEC ]] && [[ $HAD_ONCE_INTERNET -eq 0 ]]); then
+    if [[ $HAD_ONCE_INTERNET -lt 0 ]]; then HAD_ONCE_INTERNET=0; fi
+    MY_IP=$(wget -q -T 5 -O - $MYIP_SITE 2>/dev/null)
+    LAST_INTERNET_CHECK=$NOW
+    if [[ ${#MY_IP} -eq 0 ]]; then
+      echo $NOW: My ip address not available using $MYIP_SITE
+      # no IP received, check google.com
+      TEST_INTERNET=$(wget -q -T 5 -O - $ALT_SITE 2>/dev/null)
+      if [[ ${#TEST_INTERNET} -eq 0 ]]; then
+        INTERNET_ERROR=$((INTERNET_ERROR + 1))
+        echo $NOW: Internet connection error count: $INTERNET_ERROR
+      else
+        echo $NOW: Internet verified using $ALT_SITE
+        INTERNET_ERROR=0
+        HAD_ONCE_INTERNET=1
+      fi
+    else
+      echo $NOW: My ip address: $MY_IP
+      INTERNET_ERROR=0
+      HAD_ONCE_INTERNET=1
+    fi
+    if [[ $INTERNET_ERROR -ge $INTERNET_ERROR_REBOOT_AT ]]; then
+      # reboot system
+      INTERNET_ERROR=0
+      echo $NOW: Rebooting system...
+      reboot
+    fi
+  fi
+
+  LAST_CYCLE=$NOW
+
 done
